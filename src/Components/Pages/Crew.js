4c48d3e0cec4b8dc8c2681d440e3b3d6c879085c
amirrezaart <53697829+amirrezaart@users.noreply.github.com>
2021-11-30 16:46:07 +0330

    tabs and panel is done

--- src/Components/Pages/Crew.js
+++ src/Components/Pages/Crew.js
@@ -1,9 +1,34 @@
 import React from "react";
+import Navbar from "../Navigation/Navbar";
+import Panel from "../Tabs/Panel";
+import Tab from "../Tabs/Tab";
+import Viktor from "../../assets/crew/image-victor-glover.png";
+import Mark from "../../assets/crew/image-mark-shuttleworth.png";
+import Douglas from "../../assets/crew/image-douglas-hurley.png";
+import Anousheh from "../../assets/crew/image-anousheh-ansari.png";
 
 function Crew() {
+  const tabTitles = [1, 2, 3, 4];
+  const crewImages = [Viktor, Mark, Douglas, Anousheh];
   return (
-    <div>
-      <h1>CREW</h1>
+    <div className="crew">
+      <Navbar />
+      <h4>
+        <span>02</span> MIT YOUR CREW
+      </h4>
+      <Tab
+        titles={tabTitles}
+        imageSources={crewImages}
+        imageType="crew-image"
+        tabsToggleType="disk"
+      >
+        <Panel>
+          <div>HIIIIII</div>
+        </Panel>
+        <Panel>
+          <div>HIIIIII</div>
+        </Panel>
+      </Tab>
     </div>
   );
 }
--- src/Components/Pages/Destination.js
+++ src/Components/Pages/Destination.js
@@ -1,52 +1,92 @@
 import React from "react";
 import Tab from "../Tabs/Tab";
-import Navbar from './../Navigation/Navbar';
-import Panel from './../Tabs/Panel';
+import Navbar from "./../Navigation/Navbar";
+import Panel from "./../Tabs/Panel";
+import moon from "../../assets/destination/image-moon.png";
+import mars from "../../assets/destination/image-mars.png";
+import europa from "../../assets/destination/image-europa.png";
+import titan from "../../assets/destination/image-titan.png";
 
 function Destination() {
-
-  const tabTitles = ['moon', 'mars', 'europa', 'titan'];
-
+  const tabTitles = ["moon", "mars", "europa", "titan"];
+  const imageSrc = [moon, mars, europa, titan];
 
   return (
     <div className="destination">
       <Navbar />
-      <h4><span>01</span> PICK YOUR DESTINATION</h4>
-      <Tab titles={tabTitles}>
+      <h4>
+        <span>01</span> PICK YOUR DESTINATION
+      </h4>
+      <Tab titles={tabTitles} imageSources={imageSrc} imageType="planet-image">
         <Panel>
           <h1>MOON</h1>
-          <p>See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed.
-            While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
+          <p>
+            See our planet as you’ve never seen it before. A perfect relaxing
+            trip away to help regain perspective and come back refreshed. While
+            you’re there, take in some history by visiting the Luna 2 and Apollo
+            11 landing sites.
+          </p>
           <hr />
-          <p className="distance">AVG. DISTANCE <br /><span>384,400 km</span> </p>
-          <p className="travel-time">Est. travel time <br /><span>3 days</span> </p>
+          <p className="distance">
+            AVG. DISTANCE <br />
+            <span>384,400 km</span>{" "}
+          </p>
+          <p className="travel-time">
+            Est. travel time <br />
+            <span>3 days</span>{" "}
+          </p>
         </Panel>
         <Panel>
           <h1>MARS</h1>
-          <p>Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons,
-            the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!</p>
+          <p>
+            Don’t forget to pack your hiking boots. You’ll need them to tackle
+            Olympus Mons, the tallest planetary mountain in our solar system.
+            It’s two and a half times the size of Everest!
+          </p>
           <hr />
-          <p className="distance">AVG. DISTANCE <br /><span>225 MIL. km</span> </p>
-          <p className="travel-time">Est. travel time <br /><span>9 months</span> </p>
+          <p className="distance">
+            AVG. DISTANCE <br />
+            <span>225 MIL. km</span>{" "}
+          </p>
+          <p className="travel-time">
+            Est. travel time <br />
+            <span>9 months</span>{" "}
+          </p>
         </Panel>
         <Panel>
           <h1>EUROPA</h1>
-          <p>The smallest of the four Galilean moons orbiting Jupiter,
-            Europa is a winter lover’s dream. With an icy surface,
-            it’s perfect for a bit of ice skating, curling, hockey,
-            or simple relaxation in your snug wintery cabin.</p>
+          <p>
+            The smallest of the four Galilean moons orbiting Jupiter, Europa is
+            a winter lover’s dream. With an icy surface, it’s perfect for a bit
+            of ice skating, curling, hockey, or simple relaxation in your snug
+            wintery cabin.
+          </p>
           <hr />
-          <p className="distance">AVG. DISTANCE <br /><span>628 MIL. km</span> </p>
-          <p className="travel-time">Est. travel time <br /><span>3 years</span> </p>
+          <p className="distance">
+            AVG. DISTANCE <br />
+            <span>628 MIL. km</span>{" "}
+          </p>
+          <p className="travel-time">
+            Est. travel time <br />
+            <span>3 years</span>{" "}
+          </p>
         </Panel>
         <Panel>
           <h1>TITAN</h1>
-          <p>The only moon known to have a dense atmosphere other than Earth,
+          <p>
+            The only moon known to have a dense atmosphere other than Earth,
             Titan is a home away from home (just a few hundred degrees colder!).
-            As a bonus, you get striking views of the Rings of Saturn.</p>
+            As a bonus, you get striking views of the Rings of Saturn.
+          </p>
           <hr />
-          <p className="distance">AVG. DISTANCE <br /><span>1.6 BIL. km</span> </p>
-          <p className="travel-time">Est. travel time <br /><span>7 years</span> </p>
+          <p className="distance">
+            AVG. DISTANCE <br />
+            <span>1.6 BIL. km</span>{" "}
+          </p>
+          <p className="travel-time">
+            Est. travel time <br />
+            <span>7 years</span>{" "}
+          </p>
         </Panel>
       </Tab>
     </div>
